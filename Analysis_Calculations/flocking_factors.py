73ac39f122783dfa6cae2a090e162ecb6f651f53
ryanlopezzzz <ryanlopez@ucsb.edu>
2022-04-18 18:29:59 +0000

    Added velocity flocking parameter function

--- Analysis_Calculations/flocking_factors.py
+++ Analysis_Calculations/flocking_factors.py
@@ -23,3 +23,21 @@ def get_flocking_factors(exp_data, v0):
     vel_param = vel_param_nx**2 + vel_param_ny**2
     
     return vicsek_param, vel_param
+
+def get_velocity_vicsek_param(exp_data):
+    #velocity vicsek_param = | \sum \hat{v}_i / N |
+    vx_data = exp_data['vx'] #shape: [num of time snapshots, num of particles]
+    vy_data = exp_data['vy']
+    v_mag_data = np.sqrt(vx_data**2 + vy_data**2)
+    
+    #eliminate samples with zero velocity
+    vx_data = vx_data[np.all(v_mag_data!=0, axis=1)]
+    vy_data = vy_data[np.all(v_mag_data!=0, axis=1)]
+    v_mag_data = v_mag_data[np.all(v_mag_data!=0, axis=1)]
+
+    #compute order parameter
+    vx_norm_data = vx_data / v_mag_data
+    vy_norm_data = vy_data / v_mag_data
+    sum_norm_vel = np.sqrt(np.mean(vx_norm_data, 1)**2 + np.mean(vy_norm_data, 1)**2) #shape: [num of time snapshots]
+    velocity_vicsek_param = np.mean(sum_norm_vel)
+    return velocity_vicsek_param
